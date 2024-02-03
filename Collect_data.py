6857d43ce21c9f130ee77418134b97efded5a279
TheoCostes <theocostes77@gmail.com>
2024-02-03 22:53:57 +0100

    pipeline ETL lap data + connection S3

--- Collect_data.py
+++ Collect_data.py
@@ -1,7 +1,19 @@
 import pandas as pd
+import boto3
+from io import StringIO
 
 from src.api import GarminAPI, StravaAPI
 from config import GARMIN_EMAIL, GARMIN_PWD, CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN
+from config import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
+
+
+
+
+def tryconvert(value, index, default):
+    try:
+        return value.split(' - ')[index]
+    except IndexError:
+        return default
 
 
 class Collector:
@@ -16,6 +28,69 @@ class Collector:
         """
         self.garmin_api = GarminAPI(GARMIN_EMAIL, GARMIN_PWD)
         self.strava_api = StravaAPI(CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN)
+        self.s3_bucket = "datarunning"
+        self.s3_key_id = "strava_id.csv"
+        self.s3_key_activities = "strava.csv"
+        self.s3_key_lap = "strava_laps.csv"
+        self.S3 = boto3.client(
+            "s3",
+            aws_access_key_id=AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+        )
+        self.old_strava_id = self.get_old_strava_id()
+        self.old_strava_activities = self.get_old_strava_activities()
+        self.old_strava_lap = self.get_old_strava_lap()
+
+    def get_old_strava_id(self):
+        try:
+            response = self.S3.get_object(Bucket=self.s3_bucket, Key=self.s3_key_id)
+            existing_data = pd.read_csv(response['Body'])
+            return existing_data
+        except Exception as e:
+            print("ERROR :", e)
+            existing_data = pd.DataFrame()
+            return existing_data
+
+
+    def get_old_strava_activities(self):
+        try:
+            response = self.S3.get_object(Bucket=self.s3_bucket, Key=self.s3_key_activities)
+            existing_data = pd.read_csv(response['Body'])
+            return existing_data
+        except Exception as e:
+            print("ERROR :", e)
+            existing_data = pd.DataFrame()
+            return existing_data
+
+    def get_old_strava_lap(self):
+        try:
+            response = self.S3.get_object(Bucket=self.s3_bucket, Key=self.s3_key_lap)
+            existing_data = pd.read_csv(response['Body'])
+            return existing_data
+        except Exception as e:
+            print("ERROR :", e)
+            existing_data = pd.DataFrame()
+            return existing_data
+
+    def concat_and_save_strava_id(self, df_id):
+        updated_data = pd.concat([self.old_strava_id, df_id], ignore_index=True)
+        csv_buffer = StringIO()
+        updated_data.to_csv(csv_buffer, index=False)
+        self.S3.put_object(Body=csv_buffer.getvalue(), Bucket=self.s3_bucket, Key=self.s3_key_id)
+
+    def concat_and_save_strava_activities(self, df_activities):
+        print(self.old_strava_activities)
+        print(df_activities)
+        updated_data = pd.concat([self.old_strava_activities, df_activities], ignore_index=True)
+        csv_buffer = StringIO()
+        updated_data.to_csv(csv_buffer, index=False)
+        self.S3.put_object(Body=csv_buffer.getvalue(), Bucket=self.s3_bucket, Key=self.s3_key_activities)
+
+    def concat_and_save_strava_lap(self, df_lap):
+        updated_data = pd.concat([self.old_strava_lap, df_lap], ignore_index=True)
+        csv_buffer = StringIO()
+        updated_data.to_csv(csv_buffer, index=False)
+        self.S3.put_object(Body=csv_buffer.getvalue(), Bucket=self.s3_bucket, Key=self.s3_key_lap)
 
     def collect_data(self):
         """
@@ -27,15 +102,23 @@ class Collector:
         print("garmin activities fetched...")
         #self.garmin_api.save_data(garmin_activities)
         print("garmin data collected and saved...")
-        df_lap, df_activities = self.strava_api.collect_data()
+        df_lap, df_activities, df_id = self.strava_api.collect_data(list(self.old_strava_id["id"].unique()))
+
         print(type(df_activities))
         transformerActivities = TransformerActivities(df_activities)
         df_activities = transformerActivities.transform_data()
 
-        df_lap.to_csv('data/strava/strava_laps.csv', mode='a', header=False)
-        df_activities.to_csv('data/strava/strava.csv', mode='w', header=True, index=False)
+        transformerLap = TransformerLap(df_lap)
+        df_lap = transformerLap.transform_data()
+
+        self.concat_and_save_strava_activities(df_activities)
+        self.concat_and_save_strava_lap(df_lap)
+        self.concat_and_save_strava_id(df_id)
+
         print("strava data collected and saved...")
         print("finished collecting data...")
+
+
 
 
 class TransformerActivities:
@@ -43,11 +126,9 @@ class TransformerActivities:
     def __init__(self, df_activities):
         self.df_activities = df_activities
         self.columns = ['id', 'athlete', 'name', 'type', 'start_date_local', 'distance', 'moving_time',
-                        'total_elevation_gain', 'type',
-                        'sport_type', 'workout_type', 'start_date_local',
+                        'total_elevation_gain','sport_type', 'workout_type',
                         'timezone', 'utc_offset', 'achievement_count', 'kudos_count', 'comment_count',
-                        'average_speed',
-                        'max_speed', 'average_cadence', 'average_watts', 'max_watts',
+                        'average_speed','max_speed', 'average_cadence', 'average_watts', 'max_watts',
                         'weighted_average_watts', 'kilojoules', 'device_watts', 'has_heartrate',
                         'average_heartrate', 'max_heartrate', 'elev_high', 'elev_low',
                         'upload_id_str', 'external_id', 'pr_count',
@@ -55,14 +136,17 @@ class TransformerActivities:
                         ]
 
     def clean_data(self):
+        print(self.df_activities.columns)
         self.df_activities = self.df_activities[self.columns]
         self.df_activities = self.df_activities.drop_duplicates(subset=['id'])
 
     def add_feature(self):
-        self.df_activities['numero_semaine_prepa'] = self.df_activities['name'].apply(lambda x: x.split(' - ')[0])
-        self.df_activities['numero_seance_semaine'] = self.df_activities['name'].apply(lambda x: x.split(' - ')[1])
-        self.df_activities['type_seance'] = self.df_activities['name'].apply(lambda x: x.split(' - ')[3])
-        self.df_activities['prepa_name'] = self.df_activities['name'].apply(lambda x: x.split(' - ')[2])
+        self.df_activities['numero_semaine_prepa'] = self.df_activities['name'].apply(
+            lambda x: tryconvert(x, 0, "None"))
+        self.df_activities['numero_seance_semaine'] = self.df_activities['name'].apply(
+            lambda x: tryconvert(x, 1, "None"))
+        self.df_activities['type_seance'] = self.df_activities['name'].apply(lambda x: tryconvert(x, 3, "None"))
+        self.df_activities['prepa_name'] = self.df_activities['name'].apply(lambda x: tryconvert(x, 2, "None"))
 
         #convert average_speed to km/h and round to 2 decimals and also to pace
         self.df_activities['average_speed'] = self.df_activities['average_speed'] * 3.6
@@ -78,6 +162,23 @@ class TransformerActivities:
         self.add_feature()
         return self.df_activities
 
+# create a class to manage the transformation lap data
+
+class TransformerLap:
+    def __init__(self, df_lap):
+        self.df_lap = df_lap
+        self.columns = ['id', 'activity', 'athlete', 'lap_index', 'split', 'start_index', 'end_index',
+                        'moving_time', 'start_date_local', 'distance', 'average_speed', 'max_speed',
+                        'total_elevation_gain', 'average_cadence','average_watts', 'average_heartrate',
+                        'max_heartrate', 'pace_zone']
+
+    def clean_data(self):
+        self.df_lap = self.df_lap[self.columns]
+        self.df_lap = self.df_lap.drop_duplicates(subset=['id'])
+
+    def transform_data(self):
+        self.clean_data()
+        return self.df_lap
 
 if __name__ == "__main__":
     collector = Collector()
